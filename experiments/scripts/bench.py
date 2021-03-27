67d63dc364386273766aa57c63911481ba67c016
valhayot <valeriehayot@gmail.com>
2021-03-27 16:44:57 +0000

    updated benchmarks with a blocksize experiment

--- experiments/scripts/bench.py
+++ experiments/scripts/bench.py
@@ -6,17 +6,18 @@ import random
 import subprocess as sp
 from time import perf_counter
 
-output = "../results/us-west-2-xlarge/filetransferpy.bench"
 s3_path = "vhs-bucket/rand"
 
-
-def write_benchmark(fs, rep, size, time):
+def write_benchmark(output, fs, rep, size, time, blocksize=-1):
     with open(output, "a+") as f:
-        f.write(f"{fs},{rep},{size},{time}\n")
+        f.write(f"{fs},{rep},{size},{time},{blocksize}\n")
 
 
-def bench_aws(size, rep):
+def bench_aws(size, rep, output, block_size=None):
     fs = "aws"
+
+    if block_size is None:
+        block_size = size * 2 ** 20
 
     # clear caches
     helpers.drop_caches()
@@ -24,14 +25,14 @@ def bench_aws(size, rep):
     s3 = s3fs.S3FileSystem()
 
     start = perf_counter()
-    with s3.open(f"{s3_path}{size}.out", "rb", block_size= size * 2 **20) as f:
+    with s3.open(f"{s3_path}{size}.out", "rb", block_size=block_size) as f:
         data = f.read()
     end = perf_counter()
 
-    write_benchmark(fs, rep, size, end - start)
+    write_benchmark(output, fs, rep, size, end - start, block_size)
 
 
-def bench_local(size, rep, fs):
+def bench_local(size, rep, fs, output):
 
     path = f"/dev/shm/rand{size}.out"
 
@@ -50,29 +51,54 @@ def bench_local(size, rep, fs):
         data = f.read()
     end = perf_counter()
 
-    write_benchmark(fs, rep, size, end - start)
+    write_benchmark(output, fs, rep, size, end - start)
 
     # cleanup
     os.unlink(path)
 
 
-filesystems = ["aws", "local", "mem"]
-reps = 5
+def create_header(output):
 
-with open(output, "w+") as f:
-    f.write("fs,repetition,size,time\n")
+    with open(output, "w+") as f:
+        f.write("fs,repetition,size,time,blocksize\n")
 
-for r in range(reps):
-    for i in range(1, 12):
-        random.shuffle(filesystems)
 
-        for fs in filesystems:
-            size = 2 ** i
+def bench_storage():
+    filesystems = ["aws", "local", "mem"]
+    reps = 5
+    output = "../results/us-west-2-xlarge/filetransferpy.bench"
 
-            if "aws" in fs:
-                print("executing aws", r, size)
-                bench_aws(size, r)
+    create_header(output)
 
-            else:
-                print("executing", fs, r, size)
-                bench_local(size, r, fs)
+    for r in range(reps):
+        for i in range(1, 12):
+            random.shuffle(filesystems)
+
+            for fs in filesystems:
+                size = 2 ** i
+
+                if "aws" in fs:
+                    print("executing aws", r, size)
+                    bench_aws(size, r, output)
+
+                else:
+                    print("executing", fs, r, size)
+                    bench_local(size, r, fs, output)
+
+
+def bench_blocksize():
+    reps = 5
+    bsizes = [2**i for i in range(1, 12)]
+    output = "../results/us-west-2-xlarge/blocksize_s3fs.bench"
+    size = 2048
+
+    create_header(output)
+
+    for r in range(reps):
+        random.shuffle(bsizes)
+
+        for b in bsizes:
+            print("executing aws", r, size, b)
+            bench_aws(size, r, output, b*2**20)
+
+bench_blocksize()
