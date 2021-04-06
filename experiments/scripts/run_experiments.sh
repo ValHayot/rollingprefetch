b839dca4d675768ff6f2d399b37c19abf22bb1ad
valhayot <valeriehayot@gmail.com>
2021-04-06 14:36:56 +0000

    fixed exp run to have more experiments

--- experiments/scripts/run_experiments.sh
+++ experiments/scripts/run_experiments.sh
@@ -11,12 +11,16 @@ if [[ $exp == *"1"* ]]
 then
 	echo "Executing experiment 1"
 
-	python nib_read.py --block_size 8388608 --types mem --types prefetch --types s3fs --lazy
-	python nib_read.py --block_size 16777216 --types mem --types prefetch --types s3fs --lazy
-	python nib_read.py --block_size 33554432 --types mem --types prefetch --types s3fs --lazy
-	python nib_read.py --block_size 67108864 --types mem --types prefetch --types s3fs --lazy
-	python nib_read.py --block_size 134217728 --types mem --types prefetch --types s3fs --lazy
-
+	python nib_read.py --block_size 8388608 --types mem --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --block_size 16777216 --types mem --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --block_size 33554432 --types mem --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --block_size 67108864 --types mem --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --block_size 134217728 --types mem --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --block_size 268435456 --types mem --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --block_size 536870912 --types mem --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --block_size 1073741824 --types mem --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --block_size 2147483648 --types mem --types prefetch --types s3fs --lazy --reps 10
+	mv ../results/us-west-2-xlarge/readnib* ../results/us-west-2-xlarge/exp-1
 fi
 
 if [[ $exp == *"2"* ]]
@@ -25,11 +29,14 @@ then
 	#### Experiment 2 - Number of files
 	echo "Executing experiment 2"
 
-	python nib_read.py --n_files 1 --types prefetch --types s3fs --lazy
-	python nib_read.py --n_files 5 --types prefetch --types s3fs --lazy
-	python nib_read.py --n_files 10 --types prefetch --types s3fs --lazy
-	python nib_read.py --n_files 15 --types prefetch --types s3fs --lazy
-	python nib_read.py --n_files 20 --types prefetch --types s3fs --lazy
+	python nib_read.py --n_files 1 --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --n_files 5 --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --n_files 10 --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --n_files 15 --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --n_files 20 --types prefetch --types s3fs --lazy --reps 10
+	python nib_read.py --n_files 25 --types prefetch --types s3fs --lazy --reps 10
+
+	mv ../results/us-west-2-xlarge/readnib* ../results/us-west-2-xlarge/exp-2
 
 fi
 
@@ -49,26 +56,31 @@ then
 	echo "Executing experiment 4"
 
 	fs=( "s3fs" "prefetch" )
-	for i in {0..5}
+	for i in 1,5,10,15,20,25
 	do
-	    fs=( $(shuf -e "${fs[@]}") )
-	    for f in "${fs[@]}"
-	    do
-		if [ "$f" = "s3fs" ]
-		then
-		    python read_s3fs.py 0 20 $i 4 &
-		    python read_s3fs.py 20 40 $i 4 &
-		    python read_s3fs.py 40 60 $i 4 &
-		    python read_s3fs.py 60 80 $i 4 &
-		else
-		    python read_prefetched.py 0 20 $i 4 &
-		    python read_prefetched.py 20 40 $i 4 &
-		    python read_prefetched.py 40 60 $i 4 &
-		    python read_prefetched.py 60 80 $i 4 &
-		fi
+		for j in {0..10}
+		do
+		    fs=( $(shuf -e "${fs[@]}") )
+		    for f in "${fs[@]}"
+		    do
+			if [ "$f" = "s3fs" ]
+			then
+			    python read_s3fs.py 0 $i $j 4 &
+			    python read_s3fs.py $i $(( i*2 )) $j 4 &
+			    python read_s3fs.py $(( i*2 )) $(( i*3 )) $j 4 &
+			    python read_s3fs.py $(( i*3 )) $(( i*4 )) $j 4 &
+			else
+			    python read_prefetched.py 0 $i $j 4 &
+			    python read_prefetched.py $i $(( i*2 )) $j 4 &
+			    python read_prefetched.py $(( i*2 )) $(( i*3 )) $j 4 &
+			    python read_prefetched.py $(( i*3 )) $(( i*4 )) $j 4 &
+			fi
 
-		wait < <(jobs -p)
+			wait < <(jobs -p)
 
-	    done
+		    done
+		done
 	done
+	mv ../results/us-west-2-xlarge/read_prefetch* ../results/us-west-2-xlarge/exp-4
+	mv ../results/us-west-2-xlarge/read_s3fs* ../results/us-west-2-xlarge/exp-4
 fi
