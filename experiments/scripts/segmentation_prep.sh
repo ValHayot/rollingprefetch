0404ddb56ada635b41b409dab6ce9a01051793eb
valhayot <valeriehayot@gmail.com>
2021-04-28 13:49:14 +0000

    added file to get segmentation initial data

---
+++ experiments/scripts/segmentation_prep.sh
@@ -0,0 +1,4 @@
+aws s3 cp s3://vhs-bucket/dwi.nii dwi.nii
+aws s3 cp s3://hydi-tractography/analysis/mapping.nii.gz mapping.nii.gz
+wget https://ndownloader.figshare.com/files/18601661 -O hydi.bval
+wget https://ndownloader.figshare.com/files/18601658  -O hydi.bvec
